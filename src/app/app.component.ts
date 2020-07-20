7b2f4ab4089ea2ec4f287bfcf7c1a0c4e2473aed
Magdalena Sremska <m.sremska@cheil.com>
2020-07-20 15:47:44 +0200

    smart and dumb component, use @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,12 @@ export class AppComponent {
   addVote(quotation: Quotation, value: number) {
     quotation.votes += value;
   }
+
+  bestQuotes() {
+    return this.quotes.filter((q) => q.votes > 0);
+  }
+
+  worstQuotes() {
+    return this.quotes.filter((q) => q.votes < 0);
+  }
 }
---
+++ src/app/ranking/ranking.component.ts
@@ -0,0 +1,14 @@
+import { Quotation } from './../models/quotation';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ranking',
+  templateUrl: './ranking.component.html',
+  styleUrls: ['./ranking.component.css'],
+})
+export class RankingComponent {
+  @Input()
+  quotes: Quotation[];
+  @Input()
+  title: string;
+}
